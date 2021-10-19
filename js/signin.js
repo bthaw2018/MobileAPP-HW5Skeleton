bbb9c999b694dc4bedd79c4afc215767176cff4c
Benjamin Thaw <44248192+bthaw2018@users.noreply.github.com>
2021-10-19 17:13:39 +0000

    Fix issues preventing Google sign in in Android

--- js/signin.js
+++ js/signin.js
@@ -13,6 +13,18 @@ function dosignin() {
 
 }
 
+function handleAuthError(error) {
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  if (errorCode === 'auth/account-exists-with-different-credential') {
+    alert('You have already signed up with a different auth provider for that email.');
+    // If you are using multiple auth providers on your app you should handle linking
+    // the user's accounts here.
+  } else {
+    console.error(error);
+  }
+  alert('Make sure you have updated your Firebase Config in firebase_creds.js! Check console.log for more details. \n' + errorMessage);
+}
 
 function toggleSignIn() {
   //If the current user object does not exist
@@ -20,29 +32,23 @@ function toggleSignIn() {
     //Set the auth provider to Google
     var provider = new firebase.auth.GoogleAuthProvider();
     //And sign in with a popup
-    firebase.auth().signInWithPopup(provider)
-      .then(function (result) { //On Success save the token to session storage and output it to console
-        var token = result.credential.accessToken;
-        window.sessionStorage.setItem("token",token);
-        console.log(token)
+    firebase.auth().signInWithRedirect(provider)
+      .then(function () {
+        // signInWithRedirect does not provide any result
+        // We can get it with getRedirectResult()
+        firebase.auth().getRedirectResult()
+          .then(function (result) { //On Success save the token to session storage and output it to console
+            // Make sure the credential exists:
+            if (result && result.credential) {
+              var token = result.credential.accessToken;
+              window.sessionStorage.setItem("token", token);
+              console.log(token)
+            }
+          })
+          .catch(handleAuthError);
+
       })
-      .catch(function(error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        // The email of the user's account used.
-        var email = error.email;
-        // The firebase.auth.AuthCredential type that was used.
-        var credential = error.credential;
-        if (errorCode === 'auth/account-exists-with-different-credential') {
-          alert('You have already signed up with a different auth provider for that email.');
-          // If you are using multiple auth providers on your app you should handle linking
-          // the user's accounts here.
-        } else {
-          console.error(error);
-        }
-        alert('Make sure you have updated your Firebase Config in firebase_creds.js! Check console.log for more details.');
-      });
+      .catch(handleAuthError);
   } else {
     firebase.auth().signOut();
   }
